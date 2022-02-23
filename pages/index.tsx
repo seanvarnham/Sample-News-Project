35c14ff014d124ad4060442fec3f46c7220042ad
Sean Varnham <56643867+seanvarnham@users.noreply.github.com>
2022-02-23 18:09:54 +0000

    Update index.tsx
    Refactor home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,17 @@
 import Link from "next/link";
-import Footer from "../components/footer/Footer";
-import Header from "../components/header/Header";
 import PageHead from "../components/header/PageHead";
 
 const Home = () => {
 	return (
-		<div className={`wrapper`}>
-			<PageHead title="Party Planner" description="" />
-
-			<Header />
+		<>
+			<PageHead title={"No. 10 Lockdown Party Store"} description={``} />
 
 			<main className={`container`}>
 				<h1 className="">
 					Welcome to <Link href="https://nextjs.org">Next.js!</Link>
 				</h1>
 			</main>
-
-			<Footer />
-		</div>
+		</>
 	);
 };
 
