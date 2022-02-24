4c272767d83133b9b08535c843528d0688d30cc3
Sean Varnham <56643867+seanvarnham@users.noreply.github.com>
2022-02-24 20:00:28 +0000

    Update single product article markup & Styles
    Updated

--- components/layout/articles/products/ProductArticle.tsx
+++ components/layout/articles/products/ProductArticle.tsx
@@ -1,10 +1,16 @@
-// Internals
-import { Product } from "../../../../templates/interfaces";
-import classes from "./ProductArticle.module.scss";
-
 // Externals
-import { Typography } from "@material-ui/core";
+import { FormEvent, useRef, useState } from "react";
+import { useDispatch } from "react-redux";
+import Button from "@material-ui/core/Button";
+import Input from "@material-ui/core/Input";
+import Typography from "@material-ui/core/Typography";
 import Image from "next/image";
+
+// Internals
+import classes from "./ProductArticle.module.scss";
+import { Product } from "../../../../templates/interfaces";
+import { cartActions } from "../../../../store/cart/cart-slice";
+import { CartItem } from "../../../../templates/interfaces";
 
 type Props = {
 	article: Product;
@@ -42,17 +48,18 @@ const ProductPrice = (props: ProductPriceProps) => {
 	if (onSale) {
 		priceFormat = (
 			<>
-				<Typography variant="h6" variantMapping={{ h6: "p" }}>
-					{getFormattedPrice(+salePrice)}
-				</Typography>
-				<Typography
-					className={`${classes.product__price} ${classes["--prev"]}`}
-					gutterBottom
-					variant="body2"
-					variantMapping={{ body2: "p" }}
-				>
-					<s>{getFormattedPrice(+stdPrice)}</s>
-				</Typography>
+				<span className="d-flex align-center">
+					<Typography variant="h6" variantMapping={{ h6: "p" }}>
+						{getFormattedPrice(+salePrice)}
+					</Typography>
+					<Typography
+						className={`${classes.product__price} ${classes["--prev"]}`}
+						variant="body2"
+						variantMapping={{ body2: "p" }}
+					>
+						<s>{getFormattedPrice(+stdPrice)}</s>
+					</Typography>
+				</span>
 			</>
 		);
 	}
@@ -62,11 +69,31 @@ const ProductPrice = (props: ProductPriceProps) => {
 
 const ProductArticle = (props: Props) => {
 	const { article: item } = props;
+	const inputRef = useRef<HTMLInputElement>(null);
+	const dispatch = useDispatch();
+
+	const onAddToCart = (e: FormEvent<HTMLElement>) => {
+		const inputValue: any = inputRef?.current?.value;
+		const usePrice = item.onSale
+			? item.price.salePrice
+			: item.price.stdPrice;
+
+		const prodData: CartItem = {
+			id: item.id,
+			name: item.name,
+			value: usePrice * inputValue,
+			quantity: +inputValue,
+		};
+		e.preventDefault();
+
+		dispatch(cartActions.addToCart(prodData));
+	};
+
 	return (
 		<>
 			<article
 				aria-labelledby={item.name}
-				className={`cell tab-4 desk-3 ${classes.product}`}
+				className={`cell tab-4 desk-3 d-flex align-top ${classes.product}`}
 			>
 				<Image
 					layout={`intrinsic`}
@@ -75,7 +102,7 @@ const ProductArticle = (props: Props) => {
 					width={400}
 					src={item.image}
 				/>
-				<div className={classes["product__content"]}>
+				<div className={classes.product__content}>
 					<Typography
 						gutterBottom
 						variant="h5"
@@ -86,6 +113,27 @@ const ProductArticle = (props: Props) => {
 					</Typography>
 
 					<ProductPrice {...item} />
+				</div>
+				<div className={`align-self-bottom ${classes.product__footer}`}>
+					<form onSubmit={onAddToCart} className="d-flex margin-x">
+						<div className="cell mob-3">
+							<Input
+								inputRef={inputRef}
+								id={item.name}
+								type="number"
+								placeholder="0"
+							/>
+						</div>
+						<div className="cell auto d-flex align-right">
+							<Button
+								type="submit"
+								color="secondary"
+								variant="outlined"
+							>
+								Add to cart
+							</Button>
+						</div>
+					</form>
 				</div>
 			</article>
 		</>
