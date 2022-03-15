1c2f62ebcd75ee38ca652d581148b4f42d7e0269
Sean Varnham <svarnham@wundermancommerce.com>
2022-03-15 16:06:29 +0000

    New file loc'ns

---
+++ components/footer/index.tsx
@@ -0,0 +1,22 @@
+// Externals
+import { Paper } from "@material-ui/core";
+// import Link from "next/link";
+
+// Internals
+import classes from "./Footer.module.scss";
+
+const Footer = () => {
+	return (
+		<Paper
+			elevation={2}
+			component="footer"
+			className={`d-flex align-center ${classes.footer}`}
+		>
+			<div className={`container`}>
+				<span>Powered by BoJo</span>
+			</div>
+		</Paper>
+	);
+};
+
+export default Footer;
