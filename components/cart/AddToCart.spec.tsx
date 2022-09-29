0086a16ec7bfb3fd18eb29d7a5ddce3043bee60d
wtc-seanvarnham <svarnham@wundermancommerce.com>
2022-09-29 08:23:06 +0100

    Removed tests as throwing err in AWS

--- components/cart/AddToCart.spec.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from "test-utils/testing-library-utils";
-import AddToCart from "./AddToCart";
-
-describe("AddToCart Component", () => {
-	it("should do something", () => {
-		// render(<AddToCart product={}/>);
-	});
-});
